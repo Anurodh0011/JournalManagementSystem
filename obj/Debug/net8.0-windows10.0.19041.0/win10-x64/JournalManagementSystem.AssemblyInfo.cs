6b7393e4b49f0f7bcd7729cd5251dcda0005f78c
Anurodh0011 <anurodhprasain0011@gmail.com>
2026-01-07 22:19:39 +0545

    Initial Pin setup to SecureStorage and Username to Preferences and disable interaction of sidebar and appbar while not entered pin.

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/JournalManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/JournalManagementSystem.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JournalManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+95348a882c9e69563150abd14201c03bd11e51f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d6a4aa6cb4fc411624b143564f4425ceb8649812")]
 [assembly: System.Reflection.AssemblyProductAttribute("JournalManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JournalManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
