6656be758f7e130485e5bf98b3d080784cecb4ac
Anurodh0011 <anurodhprasain0011@gmail.com>
2026-01-26 11:59:18 +0545

    added obj and bin to gitignore.

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/JournalManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/JournalManagementSystem.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JournalManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+358f05853e2cdbf725ca6b02ffe9ddad273461af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f38e35231e87ab2a5f7795663c4cc97164805a52")]
 [assembly: System.Reflection.AssemblyProductAttribute("JournalManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JournalManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
