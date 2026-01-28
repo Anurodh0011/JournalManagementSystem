4e5c35391bbff94612be7e2b11bd3b49e57a4707
Anurodh0011 <anurodhprasain0011@gmail.com>
2026-01-28 17:03:29 +0545

    code optimization

--- Services/IAnalyticService.cs
+++ Services/IAnalyticService.cs
@@ -4,13 +4,6 @@ namespace JournalManagementSystem.Services;
 
 public interface IAnalyticService
 {
-    //Task<(List<JournalDisplayModel> Journals, int TotalCount)> SearchJournalsAsync(
-    //        int userId,
-    //        string searchText,
-    //        DateTime? fromDate,
-    //        DateTime? toDate,
-    //        int page,
-    //        int pageSize);
     Task<AnalyticDisplayModel> GetAnalyticsAsync(int Id);
 
     Task<DashboardSummaryModel> GetDashboardSummaryAsync(int Id);
